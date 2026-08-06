65ea8d39c419a6f30c7199854ce94a9da6553612
Rohan <your-email@example.com>
2026-08-06 12:53:10 +0530

    Fix auth config

---
+++ auth.config.ts
@@ -0,0 +1,15 @@
+import Google from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
+
+const authConfig: NextAuthConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+
+  secret: process.env.AUTH_SECRET,
+};
+
+export default authConfig;
--- src/auth.ts
+++ src/auth.ts
@@ -1,22 +1,12 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
 import { prisma } from "@/lib/prisma";
+import authConfig from "../auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-  ],
-
   session: {
     strategy: "database",
   },
-
-  secret: process.env.AUTH_SECRET,
+  ...authConfig,
 });
